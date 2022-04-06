10779e23b5c72bd38ef9f96987e6d71614cf3b07
marcosleite <marcosvpleite@gmail.com>
2022-04-06 18:16:19 -0300

    primeiros passos switch e case

---
+++ Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/primeiros-passos-switch-e-case.js
@@ -0,0 +1,17 @@
+let statusPessoaCandidata = "aprovada";
+switch(statusPessoaCandidata) {
+    case "aprovada":
+        console.log("Parabéns, você foi aprovada(o)!");
+        break;
+
+    case "lista":
+        console.log("Você está na nossa lista de espera");
+        break;
+
+    case "reprovada":
+        console.log("Você foi reprovada(o)");
+        break;
+
+    default:
+        console.log("Não se aplica");      
+}
