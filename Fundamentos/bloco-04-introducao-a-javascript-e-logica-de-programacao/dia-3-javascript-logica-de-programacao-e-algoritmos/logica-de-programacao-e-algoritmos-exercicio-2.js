0e58a303468f6271f880e74443d5e3b58088a779
marcosleite <marcosvpleite@gmail.com>
2022-04-11 16:00:39 -0300

    exercicios para fixar JavaScript - Objetos e funções

---
+++ Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/logica-de-programacao-e-algoritmos-exercicio-2.js
@@ -0,0 +1,6 @@
+//2- Agora, desenvolva um algoritmo que é capaz de inverter uma palavra. Por exemplo, a palavra "banana" seria invertida para "ananab". Utilize a string abaixo como exemplo, depois troque por outras para verificar se seu algoritmo está funcionando corretamente.
+let word = 'tryber';
+let wordInvertida;
+for (let i = 0; i < word.length; i += 1) {
+    console.log(word[i]);
+}
---
+++ Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/logica-de-programacao-e-algoritmos-exercicio-4.js
@@ -0,0 +1,10 @@
+//Um número primo é aquele divisível apenas por 1 e por ele mesmo. Sabendo disso, escreva um algoritmo que retorne o maior número primo entre 0 e 50.
+
+let maiorPrimo = 0;
+let primo = [];
+
+for (let index = 2; index < 50; index += 1) {
+    index % 2 !== 0;
+    primo.push(index);
+
+}
