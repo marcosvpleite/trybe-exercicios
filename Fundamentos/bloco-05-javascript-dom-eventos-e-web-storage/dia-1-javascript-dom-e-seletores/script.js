2f65e04b5fa8dca34410a68b60db4b1b704e7904
marcosleite <marcosvpleite@gmail.com>
2022-04-14 13:40:18 -0300

    fixando conteúdo Parte III - Qual seletor usar?

---
+++ Fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
@@ -0,0 +1,24 @@
+/*const header = document.querySelector("header");
+header.style.backgroundColor = "rgb(0,176,105)";*/
+const container = document.getElementById("container");
+container.style.backgroundColor = "#F3F3F3";
+
+const headerContainer = document.getElementById("header-container");
+headerContainer.style.backgroundColor = "rgb(0,176,105)";
+
+const h3 = document.querySelectorAll("h3");
+h3[0].style.backgroundColor = "#A500F3";
+h3[1].style.backgroundColor = "#A500F3";
+h3[2].style.backgroundColor = "black";
+h3[3].style.backgroundColor = "black";
+
+const emergencyTasks = document.getElementsByClassName("emergency-tasks");
+emergencyTasks[0].style.backgroundColor = "#FF9F84";
+
+const noEmergencyTasks = document.getElementsByClassName("no-emergency-tasks");
+noEmergencyTasks[0].style.backgroundColor = "#F9DB5E";
+
+const footerContainer = document.getElementById("footer-container");
+footerContainer.style.backgroundColor = "#003533";
+
+
