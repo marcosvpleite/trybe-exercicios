d462346348ecfd061e805cf2df17c34d649702ac
Marcos Leite <marcosvpleite@gmail.com>
2022-05-19 12:03:22 -0300

    exercicio para fixar dia 8.5

---
+++ Fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exerciciosParaFixar.js
@@ -0,0 +1,21 @@
+// Para fixar
+// Para fixar e praticar, vamos fazer uma salada de frutas com itens adicionais que você desejar. Faça uma função chamada fruitSalad passando como parâmetro specialFruit e additionalItens, faça a função retornar uma lista única contendo todos os itens da nossa salada de frutas usando o spread.
+// Faça uma lista com as suas frutas favoritas
+const specialFruit = ['banana', 'goiaba', 'uva'];
+
+// Faça uma lista de complementos que você gostaria de adicionar
+const additionalItens = ['morango', 'pêra', 'mamão'];
+
+// const fruitSalad = (fruit, additional) => {
+//   const frutas = [...specialFruit, ...additionalItens];
+//   return frutas;
+// };
+
+// console.log(fruitSalad(specialFruit, additionalItens));
+
+const fruitSalad = () => {
+    const frutas = [...specialFruit, ...additionalItens];
+    return frutas;
+  };
+  
+  console.log(fruitSalad());
