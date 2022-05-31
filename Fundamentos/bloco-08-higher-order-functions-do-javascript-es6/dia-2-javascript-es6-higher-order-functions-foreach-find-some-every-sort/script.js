3a7615370d3fc1ebc46d97ecb44080da4a80b8c5
Marcos Leite <marcosvpleite@gmail.com>
2022-05-31 14:49:37 -0300

    comite bomba

--- Fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/script.js
+++ Fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/script.js
@@ -1,17 +1,25 @@
 
 // Para fixar
 // 1 - Use o método forEach chamando a callback showEmailList para apresentar os emails
-const emailListInData = [
-    'roberta@email.com',
-    'paulo@email.com',
-    'anaroberta@email.com',
-    'fabiano@email.com',
-  ];
+// const emailListInData = [
+//     'roberta@email.com',
+//     'paulo@email.com',
+//     'anaroberta@email.com',
+//     'fabiano@email.com',
+//   ];
   
-  const showEmailList = (email) => {
-    console.log(`O email ${email} esta cadastrado em nosso banco de dados!`);
-  };
+//   const showEmailList = (email) => {
+//     console.log(`O email ${email} esta cadastrado em nosso banco de dados!`);
+//   };
   
-  // Adicione seu código aqui
-  emailListInData.forEach(showEmailList);
-  
+//   // Adicione seu código aqui
+//   emailListInData.forEach(showEmailList);
+
+const numbers = [19, 21, 30, 3, 45, 22, 15];
+
+const findDivisibleBy3And5 = () => {
+  // Adiciona seu código aqui
+  numbers.find((number) => number % 3 === 0 && number % 5 === 0 );
+};
+
+console.log(findDivisibleBy3And5());
