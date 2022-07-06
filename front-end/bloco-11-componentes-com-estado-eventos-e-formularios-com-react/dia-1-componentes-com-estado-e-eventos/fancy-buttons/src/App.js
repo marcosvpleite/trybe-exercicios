446e60dc777380998b2a44493e650711f82fde92
Marcos Leite <marcosvpleite@gmail.com>
2022-07-06 09:49:16 -0300

    exercicios de fixação do dia 1 do bloco 11

--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
@@ -5,12 +5,20 @@ import './App.css';
 class App extends React.Component {
   constructor(){
     super()
+    this.state = {
+      numeroDeCliques: 0
+    }
     this.handleClick1 = this.handleClick1.bind(this)
     this.handleClick2 = this.handleClick2 .bind(this)
     this.handleClick3 = this.handleClick3.bind(this)
   }
+
+
   handleClick1() {
     console.log("clicou no botão 1!");
+    this.setState((prevState) => ({
+      numeroDeCliques: prevState.numeroDeCliques + 1
+    }))
   }
   handleClick2() {
     console.log("clicou no botão 2!");
@@ -18,10 +26,11 @@ class App extends React.Component {
   handleClick3() {
     console.log("clicou no botão 3!");
   }
+
   render() {
     return (
       <div className="App">
-        <button onClick={this.handleClick1}>Botão 1</button>
+        <button onClick={this.handleClick1}>{`Botão 1: ${this.state.numeroDeCliques}`}</button>
         <button onClick={this.handleClick2}>Botão 2</button>
         <button onClick={this.handleClick3}>Botão 3</button>
       </div>
