ca26618aa45586f87d3f759ce20d033430190273
Marcos Leite <marcosvpleite@gmail.com>
2022-07-08 10:57:26 -0300

    exercicios de fixação do dia 1 bloco 12

---
+++ front-end/bloco-12-ciclo-de-vida-de-componentes-e-react-router/dia-1-ciclo-de-vida-de-componentes/my-interdimensional-app/src/App.js
@@ -0,0 +1,49 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state ={
+      characters:[],
+    };
+  }
+
+  fetchCharacters = () => {
+    fetch('https://rickandmortyapi.com/api/character')
+    .then(response => response.json())
+    .then(data => {
+      this.setState({
+        characters: data.results
+      })
+    })
+  }
+
+  componentDidMount() {
+    this.fetchCharacters();
+  }
+
+  render() {
+    const { characters } = this.state;
+    return (
+      <div className="App">
+        <h1>
+          Ricky and Morty Characters:
+        </h1>
+        <div className='body'>
+          { characters.map(({name, image }) => {
+            return (
+              <div className='container' key={name}>
+                  <h3>{name}</h3>
+                  <img src={image} alt={name}/>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
