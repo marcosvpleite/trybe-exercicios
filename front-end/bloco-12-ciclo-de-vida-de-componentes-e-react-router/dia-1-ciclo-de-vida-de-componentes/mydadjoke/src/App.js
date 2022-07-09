5f116d33c2bace139e38db0220246827c6e280fb
Marcos Leite <marcosvpleite@gmail.com>
2022-07-09 11:33:59 -0300

    exercicio de bloco 12 dia 1

---
+++ front-end/bloco-12-ciclo-de-vida-de-componentes-e-react-router/dia-1-ciclo-de-vida-de-componentes/mydadjoke/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Dadjoke from './Dadjoke';
+
+function App() {
+  return (
+    <div className="App">
+    
+        <Dadjoke />
+     
+    </div>
+  );
+}
+
+export default App;
