9d04176aaf1b0f4f70deff669df05b917ce7d8f8
Marcos Leite <marcosvpleite@gmail.com>
2022-10-18 16:16:31 -0300

    exercicios de fixação do dia 5 seção 4

---
+++ back-end/secao-4-introducao-ao-desenvolvimento-web-com-node-js/dia-5-node-js-express-e-mysql/trybecash-api/src/server.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+const app = require('./app');
+
+const port = 3001;
+
+app.listen(port, async () => {
+  console.log(`API TrybeCash está sendo executada na porta ${port}`);
+  
+});
